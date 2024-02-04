076647387ac9872ef3173d2a63c725a95652760b
talabidele <talabibamidele7@gmail.com>
2024-02-05 00:25:02 +0100

    Initialized tweet input

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -27,61 +27,64 @@ const TweetInput = () => {
 
 	return (
 		<div>
-			<Popover
-				returnFocusOnClose={false}
-				isOpen={isOpen}
-				onOpen={onOpen}
-				onClose={onClose}
-				placement='bottom'
-				closeOnBlur={false}
-			>
-				<Card maxW={'50vw'} margin={'2rem auto'} padding={'1rem'}>
-					<Stack divider={<StackDivider />} spacing={'3'}>
-						<Heading size={'sm'} fontWeight={'500'}>
-							Tweet something
-						</Heading>
-						<Flex>
-							<Image
-								src={userImg}
-								alt=''
-								height={'3rem'}
-								width={'3rem'}
-								objectFit={'contain'}
-								rounded={'full'}
+			<Card maxW={'50vw'} margin={'2rem auto'} padding={'1rem'}>
+				<Stack divider={<StackDivider />} spacing={'3'}>
+					<Heading size={'sm'} fontWeight={'500'}>
+						Tweet something
+					</Heading>
+					<Flex>
+						<Image
+							src={userImg}
+							alt=''
+							height={'3rem'}
+							width={'3rem'}
+							objectFit={'contain'}
+							rounded={'full'}
+						/>
+						<Box marginLeft={'1rem'} width={'full'}>
+							<Textarea
+								// value={value}
+								// onChange={handleInputChange}
+								placeholder="What's happening?"
+								size='sm'
+								variant={'unstyled'}
+								marginBottom={'1rem'}
+								resize={'none'}
 							/>
-							<Box marginLeft={'1rem'} width={'full'}>
-								<Textarea
-									// value={value}
-									// onChange={handleInputChange}
-									placeholder="What's happening?"
-									size='sm'
-									variant={'unstyled'}
-									marginBottom={'1rem'}
-								/>
+							<Popover
+								returnFocusOnClose={false}
+								isOpen={isOpen}
+								onOpen={onOpen}
+								onClose={onClose}
+								placement='bottom'
+								closeOnBlur={false}
+							>
 								<Flex alignItems={'center'} justifyContent={'space-between'}>
 									<Flex alignItems={'center'} gap={'3'} color={'#2F80ED'}>
 										<Icon as={FaRegImage} cursor={'pointer'} />
 										<PopoverTrigger>
 											<Icon as={FaGlobeAmericas} cursor={'pointer'} />
 										</PopoverTrigger>
+										<Portal>
+											<PopoverContent>
+												<PopoverHeader fontWeight='semibold'>
+													Confirmation
+												</PopoverHeader>
+												<PopoverBody>
+													Are you sure you want to continue with your action?
+												</PopoverBody>
+											</PopoverContent>
+										</Portal>
 										<Text>Everyone can reply</Text>
 									</Flex>
 
 									<Button colorScheme='blue'>Tweet</Button>
 								</Flex>
-							</Box>
-						</Flex>
-					</Stack>
-				</Card>
-				<Portal>
-					<PopoverContent>
-						<PopoverHeader fontWeight='semibold'>Confirmation</PopoverHeader>
-						<PopoverBody>
-							Are you sure you want to continue with your action?
-						</PopoverBody>
-					</PopoverContent>
-				</Portal>
-			</Popover>
+							</Popover>
+						</Box>
+					</Flex>
+				</Stack>
+			</Card>
 		</div>
 	)
 }
